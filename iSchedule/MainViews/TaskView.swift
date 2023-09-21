546d2b6179607fe0072bee2a5648598bcc81b809
Edward3957503 <s3957503@student.rmit.edu.au>
2023-09-21 16:02:56 +1000

    added sort by filter in taskview and fixed coredata relationships

--- iSchedule/MainViews/TaskView.swift
+++ iSchedule/MainViews/TaskView.swift
@@ -8,7 +8,6 @@ import SwiftUI
 import CoreData
 
 enum Status: String, Identifiable, CaseIterable {
-    
     var id: UUID {
         return UUID()
     }
@@ -18,29 +17,42 @@ enum Status: String, Identifiable, CaseIterable {
     case all = "All"
 }
 
+enum SortOption: String, CaseIterable {
+    case dateHighToLow = "Date: High to Low"
+    case dateLowToHigh = "Date: Low to High"
+}
+
 struct TaskView: View {
     
     @State private var selectedStatus: Status = .inProgress
     @Environment(\.managedObjectContext) private var viewContext
     @State private var refreshID = UUID()
     @State private var searchText = ""
+    @State private var selectedSortOption: SortOption = .dateLowToHigh
+
     var selectedTaskList: TaskList
     var fetchRequest: FetchRequest<Task>
-    
+
     init(taskList: TaskList) {
         self.selectedTaskList = taskList
         fetchRequest = FetchRequest<Task>(
             entity: Task.entity(),
-            sortDescriptors: [
-                NSSortDescriptor(key: "dueDate", ascending: true),
-                NSSortDescriptor(key: "dueTime", ascending: true)
-            ],
+            sortDescriptors: [],
             predicate: NSPredicate(format: "taskList == %@", selectedTaskList)
         )
     }
     
     var fetchedTasks: FetchedResults<Task> {
         fetchRequest.wrappedValue
+    }
+
+    var sortedTasks: [Task] {
+        switch selectedSortOption {
+        case .dateHighToLow:
+            return fetchedTasks.sorted(by: { $0.dueDate! > $1.dueDate! })
+        case .dateLowToHigh:
+            return fetchedTasks.sorted(by: { $0.dueDate! < $1.dueDate! })
+        }
     }
 
     private func deleteTask(at offsets: IndexSet) {
@@ -58,38 +70,51 @@ struct TaskView: View {
     }
 
     var body: some View {
-        
-            VStack {
-                Picker("Status", selection: $selectedStatus) {
-                    ForEach(Status.allCases) { status in
-                        Text(status.rawValue).tag(status)
-                    }
+        VStack {
+            Picker("Status", selection: $selectedStatus) {
+                ForEach(Status.allCases) { status in
+                    Text(status.rawValue).tag(status)
                 }
-                .pickerStyle(SegmentedPickerStyle())
-                
-                List {
-                    ForEach(fetchedTasks.filter { task in
-                        selectedStatus == .all || task.status == selectedStatus.rawValue
-                    }, id: \.self) { task in
-                        NavigationLink(destination: TaskSettingsView(task: task).environment(\.managedObjectContext, self.viewContext)) {
-                            TaskRow(task: task)
+            }
+            .pickerStyle(SegmentedPickerStyle())
+
+            HStack {
+                Spacer()
+                Menu {
+                    ForEach(SortOption.allCases, id: \.self) { option in
+                        Button(action: {
+                            selectedSortOption = option
+                        }) {
+                            Text(option.rawValue)
                         }
                     }
-                    .onDelete(perform: deleteTask)
+                } label: {
+                    Label("Sort by", systemImage: "arrow.up.arrow.down.circle")
                 }
-                .id(refreshID)
-                .searchable(text: $searchText)
-                
             }
-            .padding()
-            .navigationBarItems(trailing: NavigationLink(
-                destination: AddTaskView(selectedList: self.selectedTaskList)
-                                .environment(\.managedObjectContext, self.viewContext),
-                label: { Text("Add Task") })
-            )
-            .navigationTitle(selectedTaskList.name ?? "My Tasks")
+            .padding(.trailing)
+
+            List {
+                ForEach(sortedTasks.filter { task in
+                    selectedStatus == .all || task.status == selectedStatus.rawValue
+                }, id: \.self) { task in
+                    NavigationLink(destination: TaskSettingsView(task: task).environment(\.managedObjectContext, self.viewContext)) {
+                        TaskRow(task: task) // You'd define the TaskRow view elsewhere in your code.
+                    }
+                }
+                .onDelete(perform: deleteTask)
+            }
+            .id(refreshID)
+            .searchable(text: $searchText)
         }
-    
+        .padding()
+        .navigationBarItems(trailing: NavigationLink(
+            destination: AddTaskView(selectedList: self.selectedTaskList)
+                            .environment(\.managedObjectContext, self.viewContext),
+            label: { Text("Add Task") })
+        )
+        .navigationTitle(selectedTaskList.name ?? "My Tasks")
+    }
 }
 
 struct TaskView_Previews: PreviewProvider {
@@ -101,4 +126,3 @@ struct TaskView_Previews: PreviewProvider {
             .environment(\.managedObjectContext, context)
     }
 }
-
