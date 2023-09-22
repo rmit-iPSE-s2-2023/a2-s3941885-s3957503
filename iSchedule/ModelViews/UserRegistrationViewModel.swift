8e1b176472941fa072dd9880be2aea83eb6c004b
Esmat5 <s3941885@student.rmit.edu.au>
2023-09-23 07:13:11 +1000

    created password and email validation functions

--- iSchedule/ModelViews/UserRegistrationViewModel.swift
+++ iSchedule/ModelViews/UserRegistrationViewModel.swift
@@ -20,10 +20,32 @@ class UserRegistrationViewModel: ObservableObject {
         return false
     }
     
-    func passwordIsValid() -> Bool {
+    func isPasswordValid() -> Bool {
+        // https://regexlib.com/
+        // regexlib is used to evaluate password field 
         let inputPassword = NSPredicate(format: "SELF MATCHES %@", "^([a-zA-Z0-9@*#]{8,15})$")
         
-        return inputPassword.evaluate(with: password)
+        // If password is valid password ( 8 - 15 chars) and also has number (1, 2, 3...) and symbols (#, %, $...) return true
+        if inputPassword.evaluate(with: password) {
+            return true
+        }
+        // If not a valid password return false
+        else {
+            return false
+        }
+    }
+    
+    func isEmailValid() -> Bool {
+        // https://regexlib.com/
+        // regexlib is used to evaluate email field
+        let inputEmail = NSPredicate(format: "SELF MATCHES %@", "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}")
+        
+        // If email is valid return true
+        if inputEmail.evaluate(with: email){
+            return true
+        }
+        // If email is not valid return false
+        return false
     }
     
     
