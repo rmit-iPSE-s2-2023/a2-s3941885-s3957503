80dc02e545bd1d7d9d4cc92a7631483f970d9514
Esmat5 <s3941885@student.rmit.edu.au>
2023-09-23 06:51:27 +1000

    Created User Registration view model

---
+++ iSchedule/ModelViews/UserRegisterationViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserRegisterationViewModel.swift
+//  iSchedule
+//
+//  Created by Esmatullah Akhtari on 23/9/2023.
+//
+
+import Foundation
---
+++ iSchedule/ModelViews/UserRegistrationViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserRegistrationViewModel.swift
+//  iSchedule
+//
+//  Created by Esmatullah Akhtari on 23/9/2023.
+//
+
+import Foundation
