1d739c4528380e5c2fa75e36ac6675e33ba1e2c0
Esmat5 <s3941885@student.rmit.edu.au>
2023-09-29 08:03:34 +1000

    created api manager view

--- iSchedule/API Handler/APIManagerView.swift
+++ iSchedule/API Handler/APIManagerView.swift
@@ -7,3 +7,26 @@
 
 import SwiftUI
 
+func fetchData() {
+    if let url = URL(string: "https://zenquotes.io/api/random") {
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            if let data = data {
+                do {
+                    let decodedQuotes = try JSONDecoder().decode([Quote].self, from: data)
+                    DispatchQueue.main.async {
+                        self.quotes = decodedQuotes
+                    }
+                } catch {
+                    print("Error decoding JSON: \(error)")
+                }
+            } else if let error = error {
+                print("Error: \(error)")
+            }
+        }.resume()
+    }
+}
+
+struct Quote: Codable {
+    let qoute: String
+    let author: String
+}
