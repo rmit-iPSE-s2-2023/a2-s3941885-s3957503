a8de7579f094dd43aa85a14e17bde860be5b8d24
Esmat5 <s3941885@student.rmit.edu.au>
2023-10-04 15:20:54 +1100

    imported UserNotfication content to set the content of our local notification

--- iSchedule/Manager/NotificationManager.swift
+++ iSchedule/Manager/NotificationManager.swift
@@ -12,5 +12,20 @@ class NotficationManager{
     
     static func sheduleAlert(taskName: String, selectedDate: Date, selectedTime: Date, selectedAlertOption: String){
         
+        //Accessing to the content of UserNotification framework in SwiftUI to set content of the notification.
+        let content = UNMutableNotificationContent()
+        content.title = "iSchedule Reminder"
+        content.body = "Upcoming Task:\(taskName)"
+        
+        
+        //Joining the selected date and time into a single Date
+        let calendar = Calendar.current
+        let dateComponents = calendar.dateComponents([.year, .month, .day], from: selectedDate)
+        let timeComponents = calendar.dateComponents([.hour, .minute], from: selectedTime)
+        let joinedSelectedDate = calendar.date(bySettingHour: timeComponents.hour ?? 0,
+                             minute: timeComponents.minute ?? 0,
+                             second: 0,
+                             of: dateComponents.date ?? Date()) ?? Date()
+        print("Printing joinedSelectedDate:\n \(joinedSelectedDate)")
     }
 }
