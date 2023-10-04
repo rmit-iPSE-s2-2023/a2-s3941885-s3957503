6b28a4d3d6846ad021089bcd4ccd950ea5fe85b8
Esmat5 <s3941885@student.rmit.edu.au>
2023-10-04 15:05:18 +1100

    created notification manager file for implementing displying notification

---
+++ iSchedule/Manager/NotificationManager.swift
@@ -0,0 +1,14 @@
+//
+//  NotificationManager.swift
+//  iSchedule
+//
+//  Created by Esmatullah Akhtari on 4/10/2023.
+//
+
+import Foundation
+import UserNotifications
+
+class NotficationManager{
+    
+    
+}
