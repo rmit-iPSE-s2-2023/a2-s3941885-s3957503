df5f168d7399cf9f94a1d614f794e19edb53714d
Esmat5 <s3941885@student.rmit.edu.au>
2023-10-10 15:10:23 +1100

    created testIsPasswordValid unit function to validate password and tested. The test passed.

--- iScheduleTests/UserRegistrationTests.swift
+++ iScheduleTests/UserRegistrationTests.swift
@@ -6,30 +6,27 @@
 //
 
 import XCTest
+@testable import iSchedule
 
 final class UserRegistrationTest: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testIsPasswordValid() {
+        let userRegistrationModel = UserRegisterViewModel()
+        userRegistrationModel.password = "Password347"
+        
+        // Printting the password before calling isPasswordValid()
+        print("Password before validation: \(userRegistrationModel.password)")
+        
+        // Checking if the password is a valid password (length >= 8, contains Uppercase, lowercase, and number)
+        //If the password is valid, isPasswordValid() function will return true
+        let isValidPassword = userRegistrationModel.isPasswordValid()
+        
+        // Print the result
+        print("Is password valid: \(isValidPassword)")
+        
+        // If the password is valid and isPasswordValid returns true, then the test will pass.
+        // Otherwise, the test will fail and show a message that entered password is incorrect.
+        XCTAssertTrue(isValidPassword, "Password is NOT valid. Password should Contain (Uppercase, lowercanse and password must be >=8).")
     }
 
 }
