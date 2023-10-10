47dd33f36a4392c8fabaa9ba4e22f68fb7a0c7ac
Esmat5 <s3941885@student.rmit.edu.au>
2023-10-10 15:20:40 +1100

    added unit for testing invalid password

--- iScheduleTests/UserRegistrationTests.swift
+++ iScheduleTests/UserRegistrationTests.swift
@@ -11,7 +11,10 @@ import XCTest
 final class UserRegistrationTest: XCTestCase {
 
     func testIsPasswordValid() {
+        //Creating instance of UserRegistrationViewModel
         let userRegistrationModel = UserRegisterViewModel()
+        
+        //Assigning a password to password field
         userRegistrationModel.password = "Password347"
         
         // Printting the password before calling isPasswordValid()
@@ -21,7 +24,30 @@ final class UserRegistrationTest: XCTestCase {
         //If the password is valid, isPasswordValid() function will return true
         let isValidPassword = userRegistrationModel.isPasswordValid()
         
-        // Print the result
+        // Print the result of isPasswordValid is (True OR False)
+        print("Is password valid: \(isValidPassword)")
+        
+        // If the password is valid and isPasswordValid returns true, then the test will pass.
+        // Otherwise, the test will fail and show a message that entered password is incorrect.
+        XCTAssertTrue(isValidPassword, "Password is NOT valid. Password should Contain (Uppercase, lowercanse and password must be >=8).")
+    }
+    
+    //Unit Test02
+    
+    func testInvalidPassword(){
+        //Creating instance of UserRegistrationViewModel
+        let userRegistrationModel = UserRegisterViewModel()
+        
+        //Assigning a password to password field
+        userRegistrationModel.password = "valid"
+        
+        
+        // Checking if the password is a valid password (length >= 8, contains Uppercase, lowercase, and number)
+        //If the password is valid, isPasswordValid() function will return true
+        let isValidPassword = userRegistrationModel.isPasswordValid()
+        
+        // Print the result of isPasswordValid is (True OR False)
+        //The result should be False because password is not valid.
         print("Is password valid: \(isValidPassword)")
         
         // If the password is valid and isPasswordValid returns true, then the test will pass.
